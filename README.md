facaa9be8d28cd8f6008a4e7f8282d3f61917cbf
Reinertjg <jgcosta.rei4@gmail.com>
2025-04-16 00:52:09 -0300

    Criado o prejeto de Relatorio Climatico

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# clima_comparador
+A sample command-line application with an entrypoint in `bin/`, library code
+in `lib/`, and example unit test in `test/`.
---
+++ test/clima_comparador_test.dart
@@ -0,0 +1,8 @@
+import 'package:clima_comparador/clima_comparador.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
